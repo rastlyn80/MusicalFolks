e554d9a68aa33fd88c3680cbdbaba6804b55fc3b
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-10-31 00:23:14 +0000

    Created Venue Index Page

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,5 +7,11 @@ class Venue < ApplicationRecord
   validates :max_guests, presence: true
   validates :preferred_style, presence: true
   
-  
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "blank.jpg"
+    end
+  end
 end
