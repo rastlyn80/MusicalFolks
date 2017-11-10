02d5b3afa56768cbacf7278bb708ad11e718c39b
Gabriele Baldocci <gabrielebaldocci@gabrielebaldocci.com>
2017-11-10 12:38:30 +0000

    Implemented working search functionality£

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'paperclip', '~> 5.1.0'
 gem 'font-awesome-sass'
 gem 'geocoder', '~> 1.4'
 gem 'jquery-ui-rails', '~> 5.0'
+gem 'ransack', '~> 1.7'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,45 @@ class PagesController < ApplicationController
   end
 
   def search
+    # STEP 1 - Save the location in a session variable
+    if params[:search].present? && params[:search].strip != ""
+      session[:loc_search] = params[:search]
+    end
+
+    # STEP 2 - If we have a location, then return the venues near to that
+    if session[:loc_search] && session[:loc_search] != ""
+      @venues_address = Venue.where(active: true).near(session[:loc_search], 5, order: 'distance')
+    else
+      @venues_address = Venue.where(active: true).all
+    end
+
+    # STEP 3 - Use ransack to retrieve all the parameters and save the result of
+    # the search inside an array
+    @search = @venues_address.ransack(params[:q])
+    @venues = @search.result
+
+    @arrVenues = @venues.to_a
+
+    # STEP 4
+    if(params[:start_date] && params[:end_date] && !params[:start_date].empty? && !params[:end_date].empty?)
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+
+      @venues.each do |venue|
+        not_available = venue.reservations.where(
+          "(? <= start_date AND start_date <= ?)
+          OR (? <= end_date AND end_date <= ?)
+          OR (start_date < ? AND ? < end_date)",
+          start_date, end_date,
+          start_date, end_date,
+          start_date, end_date
+        ).limit(1)
+
+        if not_available.length > 0
+          @arrVenues.delete(venue)
+        end
+      end
+    end
+
   end
 end
